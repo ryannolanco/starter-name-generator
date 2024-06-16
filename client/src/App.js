c5b864ca122a454363206f00705aab6d76c31817
ryannolanco <ryannolanco@gmail.com>
2024-06-16 16:31:30 -0600

    new url fetch

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,11 @@ import React, { useState, useEffect } from 'react'
 const App = () => {
   const [name, setName] = useState([])
 
-  useEffect(() => {
-    fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
-      .then((res) => res.json())
-      .then((nme) => setName([nme]))
+  seEffect(() => {
+    fetch('https://starter-name-generator-1-krta.onrender.com') // Change this URL to the one that you just deployed
+    .then((res) => res.json())
+    .then((name) => setName([name]))
   }, [])
-
   return (
     <h1>{name}</h1>
   )
